e53c7bb91b6fca4ebceb8be2866f6661295e0d1d
Darren Royle <darren@ventureharbour.com>
2021-04-30 15:59:36 +0100

    Changing default game to connect4

--- engines/connect4.js
+++ engines/connect4.js
@@ -6,5 +6,5 @@ const GAMETYPE = "connect4";
 module.exports = () =>
   run(GAMETYPE, async (gameId, gameState) => {
     // you can just use the gamestate.board to decide what to do!
-    await playMove(gameId, GAMETYPE, { col: Math.floor(Math.random() * 6) });
+    await playMove(gameId, GAMETYPE, { col: Math.floor(Math.random() * 7) });
   });
--- main.js
+++ main.js
@@ -1,14 +1,21 @@
 const run = require("./utils/masterLooper");
 const playMove = require("./utils/playMove");
 
-const GAMETYPE = "tictactoe";
+const GAMETYPE = "connect4";
 
 run(GAMETYPE, async (gameId, gameState) => {
   // you can just use the gamestate.board to decide what to do!
   // then generate your move
+  // gameState.board = [
+  //   [0,0,0,0,0,0,0],
+  //   [0,0,0,0,0,0,0],
+  //   [0,0,0,0,0,0,0],
+  //   [0,0,0,0,0,0,0],
+  //   [0,0,0,1,0,0,0],
+  //   [0,1,2,1,2,0,0],
+  // ]
   const move = {
-    x: Math.floor(Math.random() * 2.8),
-    y: Math.floor(Math.random() * 2.8),
+    col: Math.floor(Math.random() * 7),
   };
   // for connect4 { col: Math.floor(Math.random() * 6) }
   // and play it.
